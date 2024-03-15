9109d00864ee25d61043f00a5352121c24bb1d90
Robson Maia <robsonmvieira@gmail.com>
2024-03-15 20:17:14 +0000

    news files

--- src/modules/core/Stock.Core/obj/Debug/net8.0/Stock.Core.AssemblyInfo.cs
+++ src/modules/core/Stock.Core/obj/Debug/net8.0/Stock.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stock.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0d2aeb4416f583d5532423bb5f477ccbdf9dbf4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f3c7815d1efb0670589bc572b878cf25c9541b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stock.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stock.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
